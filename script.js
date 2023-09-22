f76514884de0d1e61a0ec2ad4c10d645222c28b0
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-09-22 09:45:38 -0300

    Add hover effect

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ for (let i = 0; i < 16; i++) {
 
     for (let j = 0; j < 16; j++) {
         const square = document.createElement('div');
+
         square.style.height = '100px';
         square.style.flexGrow = 1;
         square.style.border = '1px solid black';
+
+        square.addEventListener('mouseover', () => {
+            square.classList.add('hovered');
+        })
+
         row.appendChild(square);
     }
 

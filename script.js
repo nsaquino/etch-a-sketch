4c00869fe328958406477995574b44176783e096
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-09-22 10:59:46 -0300

    Add button functionality (Change Size)

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('#btn');
+button.addEventListener('click', changeSize)
+createNewGrid(16);
 
 //Create 16x16 grid
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.style.display = 'flex';
+function createNewGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.style.display = 'flex';
 
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement('div');
 
-        square.style.flexGrow = 1;
-        square.style.aspectRatio = '1 / 1';
-        square.style.border = '1px solid black';
+            square.style.flexGrow = 1;
+            square.style.aspectRatio = '1 / 1';
+            square.style.border = '1px solid black';
 
-        square.addEventListener('mouseover', () => {
-            square.classList.add('hovered');
-        })
+            square.addEventListener('mouseover', () => {
+                square.classList.add('hovered');
+            })
 
-        row.appendChild(square);
+            row.appendChild(square);
+        }
+
+        container.appendChild(row);
     }
+}
 
-    container.appendChild(row);
+function changeSize() {
+    let input = prompt('Enter number of squares per side desired (Maximum 100):');
+    input = input.trim();
+    if (isNaN(input) || input <= 0 || input > 100) return;
+
+    //Remove old grid
+    container.innerHTML = "";
+    createNewGrid(input);
 }
